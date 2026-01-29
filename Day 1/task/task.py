6b74e511a4c429ad78742ca834a8442a84b097e8
Shaik Shoyab <smartshoyab653@gmail.com>
2026-01-29 22:51:04 +0530

    Remove Day 1 and Day 2 folders

--- Day 1/task/task.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to my program")
-city_name = input("What is the name of city you grew up?")
-pet_name = input("what is the name of your pet")
-
-print("Your band name is "+ city_name+ " "+pet_name)
--- Day 2/Type Error, Checking and Conversion/task.py
+++
@@ -1,6 +0,0 @@
-
-name = input("Enter your name")
-length = len(name)
-print(type(name))
-print(type(length))
-print("Number of letters in your name: "+ str(length) )

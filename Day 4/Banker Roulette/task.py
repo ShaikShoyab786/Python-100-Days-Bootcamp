f04f018538aedc7220313faa493525570e35d1b8
Shaik Shoyab <smartshoyab653@gmail.com>
2026-01-29 22:58:04 +0530

    previous folders deleted

--- Day 4/Banker Roulette/task.py
+++
@@ -1,7 +0,0 @@
-import random
-
-rand_num = random.randint(0, 4)
-print(rand_num)
-
-friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
-print(friends[rand_num])
--- Day 5/For Loops with Range/task.py
+++
@@ -1,5 +0,0 @@
-
-sum =0
-for nums in range(1,101):
-    sum += nums
-print(sum)
